564745ee5268cbec7ab98aeedce4576e2c747237
gabilgg07 <gabilgg07@gmail.com>
2023-04-14 19:20:25 +0400

    mix_3 Publish .WebApi, SwaggerOperation, Swagger Annotations

--- fuodborne-react/src/components/Pages/Contact.js
+++ fuodborne-react/src/components/Pages/Contact.js
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 
 const Contact = () => {
   return (
-    <div class="contact-page">
+    <div className="contact-page">
       <header
         id="page-top"
-        class="blog-banner"
+        className="blog-banner"
         style={{
           backgroundImage:
             "url('https://tidytheme.com/blackgallery/FuodBorne-react/assets/images/header.jpg')",
         }}
       >
-        <div class="container" id="blog">
-          <div class="row blog-header text-center">
-            <div class="col-sm-12">
+        <div className="container" id="blog">
+          <div className="row blog-header text-center">
+            <div className="col-sm-12">
               <h3>Contact Us</h3>
               <h4>
                 <Link to="/"> Home </Link> &gt; Contact Us
@@ -23,66 +23,66 @@ const Contact = () => {
           </div>
         </div>
       </header>
-      <section class="contact-section">
-        <div class="container">
-          <div class="base-header">
+      <section className="contact-section">
+        <div className="container">
+          <div className="base-header">
             <small>Get in touch</small>
             <h3>Send Us Message</h3>
           </div>
-          <div class="contact_wrp">
-            <div class="row">
-              <div class="col-md-8 col-sm-12 inner-contact">
-                <div class="contact-form">
+          <div className="contact_wrp">
+            <div className="row">
+              <div className="col-md-8 col-sm-12 inner-contact">
+                <div className="contact-form">
                   <div id="message"></div>
                   <form method="post" name="contact-form" id="contact-form">
-                    <div class="row">
-                      <div class="col-lg-6 col-sm-12">
+                    <div className="row">
+                      <div className="col-lg-6 col-sm-12">
                         <input
                           type="text"
                           id="name"
-                          class="con-field form-control"
+                          className="con-field form-control"
                           placeholder="Name"
                           value=""
                         />
                         <span id="err"></span>
                       </div>
-                      <div class="col-lg-6 col-sm-12">
+                      <div className="col-lg-6 col-sm-12">
                         <input
                           type="text"
-                          class="con-field form-control"
+                          className="con-field form-control"
                           id="exampleInputEmail1"
                           placeholder="Email"
                           value=""
                         />
                         <span id="err"></span>
                       </div>
-                      <div class="col-lg-12 col-sm-12">
+                      <div className="col-lg-12 col-sm-12">
                         <input
                           type="text"
                           id="subject"
-                          class="form-control con-field"
+                          className="form-control con-field"
                           placeholder="Your Subject.."
                           value=""
                         />
                         <span id="err"></span>
                       </div>
                     </div>
-                    <div class="row">
-                      <div class="col-lg-12 col-sm-12">
+                    <div className="row">
+                      <div className="col-lg-12 col-sm-12">
                         <textarea
                           name="comments"
                           id="comments"
                           rows="6"
-                          class="form-control con-field"
+                          className="form-control con-field"
                           placeholder="Your Message"
                         ></textarea>
                         <span id="err"></span>
-                        <div class="submit-area">
+                        <div className="submit-area">
                           <input
                             type="button"
                             id="submit"
                             name="send"
-                            class="submit-contact submitBnt"
+                            className="submit-contact submitBnt"
                             value="Send Message"
                           />
                           <div id="simple-msg"></div>
@@ -92,15 +92,15 @@ const Contact = () => {
                   </form>
                 </div>
               </div>
-              <div class="col-md-4 col-sm-12">
-                <div class="contact_pg_address">
+              <div className="col-md-4 col-sm-12">
+                <div className="contact_pg_address">
                   <h3>
                     Hello there <br />
                     got a project?
                   </h3>
-                  <div class="single_con_add">
+                  <div className="single_con_add">
                     <a href="#/">
-                      <i class="icon-glyph-226"></i>
+                      <i className="icon-glyph-226"></i>
                     </a>
                     <p>
                       <span>Company Address :</span>
@@ -109,9 +109,9 @@ const Contact = () => {
                       <span>3567 New Alaska, us</span>
                     </p>
                   </div>
-                  <div class="single_con_add">
+                  <div className="single_con_add">
                     <a href="#/">
-                      <i class="icon-glyph-334"></i>
+                      <i className="icon-glyph-334"></i>
                     </a>
                     <p>
                       <span>Email Address :</span>
@@ -120,9 +120,9 @@ const Contact = () => {
                       <span>example@email .com</span>
                     </p>
                   </div>
-                  <div class="single_con_add">
+                  <div className="single_con_add">
                     <a href="#/">
-                      <i class="icon-glyph-33"></i>
+                      <i className="icon-glyph-33"></i>
                     </a>
                     <p>
                       <span>Contact Us :</span>
--- fuodborne-react/src/components/Services/Services.js
+++ fuodborne-react/src/components/Services/Services.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import API from "../../api/api";
 
 const Services = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    const getServices = async () => {
+      const { data } = await API.get("/services");
+      setServices(data);
+    };
+    getServices();
+  }, []);
+
   return (
     <>
       <section className="service-section">
@@ -36,93 +47,34 @@ const Services = () => {
             <h3>Service we offer</h3>
           </div>
           <div className="row">
-            <div className="col-md-4 col-sm-12">
-              <div className="service-item">
-                <div className="img_serv">
-                  <a href="/blackgallery/FuodBorne-react/services">
-                    <img
-                      src="https://tidytheme.com/blackgallery/FuodBorne-react/assets/images/service_1.jpg"
-                      alt="service"
-                    />
-                  </a>
+            {services.length > 0 &&
+              services.map((s) => (
+                <div key={s.id} className="col-md-4 col-sm-12">
+                  <div className="service-item">
+                    <div className="img_serv">
+                      <a href="/blackgallery/FuodBorne-react/services">
+                        <img
+                          src={`https://tidytheme.com/blackgallery/FuodBorne-react/assets/images/${s.imageUrl}`}
+                          alt="service"
+                        />
+                      </a>
+                    </div>
+                    <div className="service_text">
+                      <a href="/blackgallery/FuodBorne-react/services">
+                        {" "}
+                        <h4>{s.name}</h4>
+                      </a>
+                      <p>{s.description}</p>
+                    </div>
+                    <a
+                      className="serv_link"
+                      href="/blackgallery/FuodBorne-react/services"
+                    >
+                      <i className="icon-glyph-40"></i>
+                    </a>
+                  </div>
                 </div>
-                <div className="service_text">
-                  <a href="/blackgallery/FuodBorne-react/services">
-                    {" "}
-                    <h4>Fresh Fruits</h4>
-                  </a>
-                  <p>
-                    Organically grown crops tend use natural fertilizers like
-                    manure to improve growth to plant . Animals raised
-                    organically are
-                  </p>
-                </div>
-                <a
-                  className="serv_link"
-                  href="/blackgallery/FuodBorne-react/services"
-                >
-                  <i className="icon-glyph-40"></i>
-                </a>
-              </div>
-            </div>
-            <div className="col-md-4 col-sm-12">
-              <div className="service-item">
-                <div className="img_serv">
-                  <a href="/blackgallery/FuodBorne-react/services">
-                    <img
-                      src="https://tidytheme.com/blackgallery/FuodBorne-react/assets/images/service_2.jpg"
-                      alt="service"
-                    />
-                  </a>
-                </div>
-                <div className="service_text">
-                  <a href="/blackgallery/FuodBorne-react/services">
-                    {" "}
-                    <h4>Fresh Vegetable</h4>
-                  </a>
-                  <p>
-                    Organically grown crops tend use natural fertilizers like
-                    manure to improve growth to plant . Animals raised
-                    organically are
-                  </p>
-                </div>
-                <a
-                  className="serv_link"
-                  href="/blackgallery/FuodBorne-react/services"
-                >
-                  <i className="icon-glyph-40"></i>
-                </a>
-              </div>
-            </div>
-            <div className="col-md-4 col-sm-12">
-              <div className="service-item">
-                <div className="img_serv">
-                  <a href="/blackgallery/FuodBorne-react/services">
-                    <img
-                      src="https://tidytheme.com/blackgallery/FuodBorne-react/assets/images/service_3.jpg"
-                      alt="service"
-                    />
-                  </a>
-                </div>
-                <div className="service_text">
-                  <a href="/blackgallery/FuodBorne-react/services">
-                    {" "}
-                    <h4>Natural Wheats</h4>
-                  </a>
-                  <p>
-                    Organically grown crops tend use natural fertilizers like
-                    manure to improve growth to plant . Animals raised
-                    organically are
-                  </p>
-                </div>
-                <a
-                  className="serv_link"
-                  href="/blackgallery/FuodBorne-react/services"
-                >
-                  <i className="icon-glyph-40"></i>
-                </a>
-              </div>
-            </div>
+              ))}
           </div>
         </div>
       </section>
